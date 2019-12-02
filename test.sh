5b38e8989b1d55c9319cecb1703e5b7e2781085d
teor <teor@torproject.org>
2019-12-02 14:05:17 +1000

    test: Use single # for comments
    And move a comment.
    
    Part of 28863.

--- test.sh
+++ test.sh
@@ -13,9 +13,10 @@ if command -v shellcheck; then
     find . -name "*.sh" -exec shellcheck "$EXCLUSIONS" {} +
 fi
 
+# TODO: validate output from all 3 commands using grep, grep, and stem?
 
 # List of relay fingerprints to test with generate and lookup
-## moria1, Serge, no caches extra info, no dir port, doesn't exist
+# moria1, Serge, no caches extra info, no dir port, doesn't exist
 TEST_RELAY_LIST="
 9695DFC35FFEB861329B9F1AB04C46397020CE31
 BA44A889E64B93FAA2B114E02C2A279A8555C533
@@ -24,15 +25,14 @@ BA44A889E64B93FAA2B114E02C2A279A8555C533
 AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 "
 
-## TODO: validate output from all 3 commands using grep, grep, and stem?
 # We need each relay fingerprint as a separate argument
 # shellcheck disable=SC2086
 $PYTHON generateFallbackDirLine.py $TEST_RELAY_LIST
 # shellcheck disable=SC2086
 $PYTHON lookupFallbackDirContact.py $TEST_RELAY_LIST
 
-## If we get the top 200 relays from Onionoo, we generate a list with about
-## 10 fallbacks
+# If we get the top 200 relays from Onionoo, we generate a list with about
+# 10 fallbacks
 export TOR_FB_ONIONOO_LIMIT=200
-## Hide info-level logs
+# Hide info-level logs
 $PYTHON updateFallbackDirs.py 2>&1 | tee -a fallback.log | grep -v "INFO::"
